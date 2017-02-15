6a3f4a6f0b1bb1e3de0a6ceca07d124b00d37231
Will <wlhaskell@gmail.com>
2017-02-15 15:55:35 +0000

    finished Shoot class

--- rps.rb
+++ rps.rb
@@ -1,4 +1,8 @@
 class Shoot
+	
+	ttr_reader :name
+
+	@@shots = []
 
 	def initialize(name)
 		@name = name
@@ -7,11 +11,25 @@ class Shoot
 	end
 
 	def beats(*args)
-
+		args.each do |arg|
+			if !@beats.include? arg
+				@beats.push arg
+			end
+		end
 	end
 
 	def <=>(shot)
-
+		if shot.class == Shoot
+			if shot == self
+				0
+			elsif  @beats.include? shot
+				1
+			else
+				-1
+			end
+		else
+			nil
+		end
 	end
 
 	def self.shots
