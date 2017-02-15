5494f188d97689ec2dd74f72f0c876f2241b87da
Will <wlhaskell@gmail.com>
2017-02-15 16:42:54 +0000

    completed RPS

--- rps.rb
+++ rps.rb
@@ -37,3 +37,46 @@ class Shoot
 	end
 
 end
+
+
+rock = Shoot.new(:rock)
+paper = Shoot.new(:paper)
+scissor = Shoot.new(:scissor)
+
+rock.beats(scissor)
+paper.beats(rock)
+scissor.beats(paper)
+
+again = true
+
+while again
+	"\n Shots:"
+	Shoot.shots.each_with_index do |shot, i|
+		puts "#{i+1}. #{shot.name.capitalize}"
+	end
+
+	print "\nInput Shot:"
+	player_shot = Shoot.shots[gets.chomp.to_i - 1]
+	computer_shot = Shoot.shots[rand(0..4)]
+
+	puts "\n#{player_shot.name} vs. #{computer_shot.name}"
+
+	case player_shot <=> computer_shot
+	when 0
+		result = "tie"
+	when 1
+		result = "win"
+	when -1
+		result = "lose"
+	end
+
+	puts "You #{result}!\n"
+
+	puts "\nPlay again?"
+	input = gets.chomp
+	if input[0].capitalize == "Y"
+		again = true
+	else
+		again = false
+	end
+end
